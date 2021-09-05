d3f534d6fdcbeedb2469a07994e1208a44666e51
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-09-05 15:54:14 +0100

    List displaying with photos

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -6,7 +6,21 @@ const CharacterItem = ({character, onCharacterClick}) => {
         onCharacterClick(character);
     }
 
-    return <li onClick={handleClick}>{character.name}</li>
+    const pictureStyle = {
+       height: '200px',
+       width: 'auto'
+    }
+
+    return (
+        <div>
+            <img style={pictureStyle} src={character.img}/>
+            <li onClick={handleClick}>Character: {character.name}</li>
+            <li onClick={handleClick}>Nickname: {character.nickname}</li>
+            <li onClick={handleClick}>Fate: {character.status}</li>
+            <li onClick={handleClick}>Played by: {character.portrayed}</li>
+        </div>
+    )
+
 }
 
 export default CharacterItem;
